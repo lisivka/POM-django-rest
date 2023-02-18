183fc6ced31787caadbd842e0b7af9b10f4f6904
lisivka <lisivka@gmail.com>
2023-02-18 20:35:38 +0200

    Add pagination for API#11

--- library/library/settings.py
+++ library/library/settings.py
@@ -178,5 +178,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGINATE_BY': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 10
 }
